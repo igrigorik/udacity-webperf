9abe011149d33b72fc3c25fc833a8629e9e40949
Ilya Grigorik <ilya@igvita.com>
2014-04-16 16:31:20 -0700

    expose rtt/processing flags in server

--- l1-critical-rendering-path/server.rb
+++ l1-critical-rendering-path/server.rb
@@ -1,24 +1,22 @@
 require 'goliath'
 
 class Srv < Goliath::API
+  use Goliath::Rack::Params
+
   def response(env)
     file = env['REQUEST_PATH'].split('/')[1]
-    # type = 'text/html'
 
-    EM::Synchrony.sleep(0.1)
+    rtt  = env.params.fetch('rtt', 0.1).to_f
+    processing = env.params.fetch('processing', 0.01).to_f
+    EM::Synchrony.sleep(rtt + processing)
 
     type = case file
     when /\.html$/
-      EM::Synchrony.sleep(0.1)
       'text/html'
     when /\.css$/
-      EM::Synchrony.sleep(0.01)
       'text/css'
     when /\.js$/
-      EM::Synchrony.sleep(0.01)
       'application/javascript'
-    else
-      EM::Synchrony.sleep(0.01)
     end
 
     [200, {'Content-Type' => type}, IO.read(file)]
